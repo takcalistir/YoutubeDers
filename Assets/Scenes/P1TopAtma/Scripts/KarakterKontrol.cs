30358d73114040b1114e7352dd55dac9f192a3ae
S Programcı <sinasalik@hotmail.com>
2025-01-16 10:02:59 +0300

    Initial commit

---
+++ Assets/Scenes/P1TopAtma/Scripts/KarakterKontrol.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class KarakterKontrol : MonoBehaviour
+{
+    public float hiz;
+    public float fareHassasiyet;
+    public float fareBakisLimit;
+
+    Rigidbody karakter;
+    float xBakis = 0.0f;
+
+    void Start()
+    {
+        karakter = GetComponent<Rigidbody>();
+
+        // Mouse imlecini gizler.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Hareket();
+        FareYon();
+    }
+
+    void Hareket()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+
+        Vector3 hareketYonu = new Vector3(yatay, 0.0f, dikey).normalized;
+
+        if(hareketYonu.magnitude > 0.1f)
+        {
+            float kosma = Input.GetKey(KeyCode.LeftShift) ? 2.0f : 1.0f;
+
+            Vector3 yeniPozisyon = hareketYonu * kosma * hiz * Time.deltaTime;
+            karakter.MovePosition(karakter.position + transform.TransformDirection(yeniPozisyon));
+        }
+    }
+
+    void FareYon()
+    {
+        float fareX = Input.GetAxis("Mouse X");
+        float fareY = Input.GetAxis("Mouse Y");
+
+        xBakis -= fareY;
+
+        xBakis = Mathf.Clamp(xBakis, -fareBakisLimit, fareBakisLimit);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xBakis, 0.0f, 0.0f);
+
+        transform.Rotate(Vector3.up * fareX);
+    }
+}
---
+++ Assets/Scenes/P1TopAtma/Scripts/NesneKontrol.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NesneKontrol : MonoBehaviour
+{
+    public Image nisangah;
+    Color renkAktif = Color.white;
+    Color renkPasif = Color.green;
+
+    public Transform tutusNokta;
+    public float atisHizi;
+
+    Rigidbody nesne;
+    Camera kamera;
+    void Start()
+    {
+        kamera = transform.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        NesneTara();
+
+        if(nesne != null)
+        {
+            NesneTut();
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                NesneAt();
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                NesneAt(false);
+            }
+        }
+    }
+
+    void NesneTara()
+    {
+        Ray ray = kamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Kup"))
+                {
+                    if (Input.GetMouseButton(0) && nesne == null)
+                    {
+                        nesne = hit.collider.GetComponent<Rigidbody>();
+                        nesne.useGravity = false;
+                        nesne.constraints = RigidbodyConstraints.FreezeRotation;
+                    }
+
+                    nisangah.color = renkAktif;
+                    return;
+                }
+            }
+        }
+
+        nisangah.color = renkPasif;
+    }
+
+    void NesneTut()
+    {
+        if(tutusNokta != null)
+        {
+            nesne.linearVelocity = (tutusNokta.position - nesne.position) * 10f;
+        }
+    }
+
+    void NesneAt(bool nesneAt = true)
+    {
+        nesne.useGravity = true;
+        nesne.constraints = RigidbodyConstraints.None;
+
+        if(nesneAt)
+            nesne.AddForce(kamera.transform.forward * atisHizi, ForceMode.Impulse);
+
+        nesne = null;
+    }
+}
